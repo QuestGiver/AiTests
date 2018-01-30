6162af92c4b25d398d40bef18851a35b0da97b03
Paul Gibbons <s179054@students.aie.edu.au>
2018-01-29 16:20:19 -0800

    First Commit
    *Clink* Here's to a bright future of messing with Ai.

---
+++ Assets/Scripts/RandFlee.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandFlee : MonoBehaviour
+{
+    public float speed;
+    public float walkRange;
+    public Transform enemy;
+
+    public float wanderRefreshTimer;
+    float timer;
+
+    Vector3 desiredVel;
+
+
+    Rigidbody myRig;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        myRig = GetComponent<Rigidbody>();
+        desiredVel = speed * (transform.position - (enemy.transform.position + NewDestination())).normalized;
+    }
+
+    Vector3 randoDest;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderRefreshTimer)
+        {
+            desiredVel = speed * ((transform.position - (enemy.transform.position + NewDestination()))).normalized;
+            
+            timer = 0;
+        }
+
+        myRig.AddForce((desiredVel ) * Time.deltaTime);
+        //Debug.Log(desiredVel);
+        Debug.DrawLine(transform.position, transform.position + (desiredVel * 10), Color.red);
+        Debug.DrawLine(enemy.transform.position, enemy.transform.position + randoDest, Color.green);
+    }
+
+
+    Vector3 NewDestination()
+    {
+        Vector3 retval = new Vector3(Random.Range(-walkRange, walkRange), 0, Random.Range(-walkRange, walkRange));
+        randoDest = retval;
+        return retval;
+    }
+
+}
